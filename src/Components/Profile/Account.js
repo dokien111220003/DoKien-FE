1902ab0dff3d0bece7b5b10708008446952c1d94
dokien111220003 <118035034+dokien111220003@users.noreply.github.com>
2024-04-25 18:52:58 +0700

    fasfaf

--- src/Components/Profile/Account.js
+++ src/Components/Profile/Account.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { AiFillPicture } from "react-icons/ai";
 import * as message from '../Message/Message'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Account = () => {
     const [showPassword] = useState(false);
@@ -14,22 +16,22 @@ const Account = () => {
     const dispatch = useDispatch()
     const [image, setImage] = useState(null);
     const [imageName, setImageName] = useState("No selected image");
-    console.log('user', user)
+
     const handleGenderChange = (e) => {
         setGender(e.target.value);
     };
 
     const handleUpload = () => {
         message.success()
-        
-      };
+        toast.success('Profile updated successfully!');
+    };
 
     const handleImageChange = ({ target }) => {
         const image = target.files[0];
         if (image) {
-          setImage(image);
+            setImage(image);
         }
-      };
+    };
     return (
         <div className="acc-container">
             <div className='acc-information-container'>
@@ -43,61 +45,51 @@ const Account = () => {
                     </div>
                     <div className="input-acc">
                         <label>Phone:</label>
-                        <input type="number" value={user.phone} readOnly />
+                        <input type="text" value={user.phone} readOnly />
                     </div>
                     <div className="input-acc-faculty">
                         <label>Faculty:</label>
-                        <input type="text" value={user.faculty} readOnly />
+                        <input type="text" value={user.faculty.name} readOnly />
                     </div>
                     <div className="input-acc">
                         <label>Address:</label>
                         <input type="text" value={user.address} readOnly />
                     </div>
-                    <div className="checkbox-gender">
-                        <label><input type="checkbox" value="male" checked={gender === 'male'} onChange={handleGenderChange} />Male</label>
-                        <label><input type="checkbox" value="female" checked={gender === 'female'} onChange={handleGenderChange} />Female</label>
-                        <div className="change-pass">
-                            <a href="change_password">Change Password</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="account-submit-container">
-                    <button className="account-submit">UPDATE</button>
                 </div>
             </div>
             <div className="acc-image-container">
                 <div className="acc-image-header">
                     <div className="acc-text">Your Photo</div>
                 </div>
-                <div className="acc-image">
-                {image ? (
-    <div className="image-preview">
-      <img 
-        src={URL.createObjectURL(image)} 
-        alt="Preview" 
-        style={{
-          width: "100%",        
-          maxHeight: "300px",   
-          objectFit: "contain",
-          marginTop: "10px",
-          border: "1px solid #ccc", 
-          padding: "10px",     
-          boxSizing: "border-box" 
-        }} 
-      />
-    </div>
-  ) : (
-    <>
-      <AiFillPicture color="#1475cf" size={60} />
-      <p>Browse Files to Upload Images</p>
-    </>
-  )}
-                    <input type="file" onChange={handleImageChange} accept="image/*"/>
+                <div className="acc-image-hehe">
+                    {image ? (
+                        <div className="image-preview">
+                            <img
+                                src={URL.createObjectURL(image)}
+                                alt="Preview"
+                                style={{
+                                    width: "100%",
+                                    maxHeight: "300px",
+                                    objectFit: "contain",
+                                    marginTop: "10px",
+                                    border: "1px solid #ccc",
+                                    boxSizing: "border-box"
+                                }}
+                            />
+                        </div>
+                    ) : (
+                        <>
+                            <AiFillPicture color="#1475cf" size={60} />
+                            <p>Browse Files to Upload Images</p>
+                        </>
+                    )}
+                    <input type="file" onChange={handleImageChange} accept="image/*" className="profile-input" />
                 </div>
                 <div className="account-submit-container">
                     <button className="account-img-submit" onClick={handleUpload}>UPDATE</button>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     )
 }
